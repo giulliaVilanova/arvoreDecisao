e7eb917f5b01baa3ed3b06a3439e39a5a173790a
giulliaVilanova <govilanova@gmail.com>
2024-11-10 22:06:34 -0300

    Update generateData.py

--- generateData.py
+++ generateData.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import random
 
-# Definindo os valores para cada atributo
 idades = ["Jovem", "Adulto", "Idoso"]
 rendas = ["Baixa", "Média", "Alta"]
 localizacoes = ["Urbano", "Rural"]
 
-# Gerando uma lista de dicionários para os objetos (linhas da tabela)
 dados = [
     {
         "Idade": random.choice(idades),
@@ -16,10 +14,8 @@ dados = [
     for _ in range(40)
 ]
 
-# Criando o DataFrame
 df = pd.DataFrame(dados)
 
-# Salvando em um arquivo Excel na pasta atual
 output_path = "dados_para_arvore_decisao.xlsx"
 df.to_excel(output_path, index=False)
 
